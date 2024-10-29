b4d3c9c3c0e4f116ab46b7f6097fafce30cb7e18
fisayo-oluwayemi <fisayo@kindredteam.com>
2024-10-29 12:34:32 +0100

    intialized simple storage project and added state variables

---
+++ src/Storage.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract SimpleStorage {
+    bool hasFaouriteNumber = false;
+    uint256 favouriteNumber = 90;
+    string favouriteNumbebrInText = "ninety";
+    address myAddress;
+}
